d80d01c56ad7e811aad507b862b49df13709ea21
Anique <aniquekhan004@gmail.com>
2023-08-17 15:35:16 +0500

    still work to do on authentication

--- food_order/app_authentication/models.py
+++ food_order/app_authentication/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class UserAccount(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
+    phone_number = models.CharField(max_length=20,blank=True,null=True)
+    address = models.CharField(max_length=255,blank=True,null=True)
+
+
+    def __str__(self):
+        return f'{self.user}'
--- food_order/app_authentication/views.py
+++ food_order/app_authentication/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.contrib.auth import login
+from django.shortcuts import render, redirect
+from app_authentication.forms import SignUpForm
+
+
 def signup(request):
-    return render(request,"app_authentication/signup.html")
+    if request.method == 'POST':
+        form = SignUpForm(request.POST, request.FILES)
+        if form.is_valid():
+            print("form is valid")
+            return redirect('index')  # Redirect to the desired page after successful signup
+    else:
+        form = SignUpForm()
+    return render(request, 'app_authentication/signup.html', {'form': form})
+
 
 
 def login(request):
