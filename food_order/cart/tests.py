34de5f0e583847316b6ef160407807fe704ec60e
Anique <68085660+AniqueKhan@users.noreply.github.com>
2023-08-24 14:52:57 +0500

    incomplete tests from remote server

--- food_order/cart/tests.py
+++ food_order/cart/tests.py
@@ -1,3 +1,57 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
+from food.models import Dish, Sales,Restaurant
+from cart.models import Cart, CartItem
+from datetime import date,timedelta
 
-# Create your tests here.
+class CartViewTests(TestCase):
+    def setUp(self):
+        self.restaurant = Restaurant.objects.create(name="Test Restaurant")
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+        self.client.login(username='testuser', password='testpassword')
+        self.cart = Cart.objects.create(user=self.user)
+        self.dish = Dish.objects.create(name='Test Dish', price=10.00,restaurant=self.restaurant)
+        self.sale = Sales.objects.create(
+            restaurant=self.restaurant,
+            title="Test Sale",
+            discount_percentage=20,
+            start_date=date.today() - timedelta(days=1),
+            end_date=date.today() + timedelta(days=1),
+        )
+        self.dish.sales.add(self.sale)
+        
+    def test_add_to_cart(self):
+        url = reverse('add_to_cart', args=[self.dish.id])
+        response = self.client.get(url)
+        
+        self.assertEqual(response.status_code, 302)  # Check if the response redirects
+        self.assertEqual(self.cart.cartitem_set.count(), 1)  # Check if the cart item is added
+        
+    def test_remove_from_cart(self):
+        cart_item = CartItem.objects.create(cart=self.cart, dish=self.dish, quantity=2)
+        url = reverse('remove_from_cart', args=[cart_item.id])
+        response = self.client.get(url)
+        
+        self.assertEqual(response.status_code, 302)  # Check if the response redirects
+        # Try to get the cart item, it should raise a DoesNotExist exception
+        with self.assertRaises(CartItem.DoesNotExist):
+            CartItem.objects.get(id=cart_item.id)  
+        
+    def test_update_cart(self):
+        cart_item = CartItem.objects.create(cart=self.cart, dish=self.dish, quantity=2)
+        url = reverse('update_cart', args=[cart_item.id])
+        data = {str(cart_item.id): '3'}
+        response = self.client.post(url, data)
+        
+        self.assertEqual(response.status_code, 302)  # Check if the response redirects
+        updated_cart_item = CartItem.objects.get(id=cart_item.id)
+        self.assertEqual(updated_cart_item.quantity, 3)  # Check if the quantity is updated
+        
+    def test_view_cart(self):
+        url = reverse('view_cart')
+        response = self.client.get(url)
+        
+        self.assertEqual(response.status_code, 200)  # Check if the response is successful
+        self.assertTemplateUsed(response, 'cart/view_cart.html')  # Check if the correct template is used
+
--- food_order/food/tests.py
+++ food_order/food/tests.py
@@ -1,8 +1,11 @@
-from django.test import TestCase
+from django.test import TestCase,Client
 from food.models import Restaurant,Dish,DishCategory,Sales
 from decimal import Decimal
+from django.urls import reverse
+from django.contrib.auth.models import User
 from datetime import date,timedelta
 from django.core.exceptions import ValidationError
+from food.forms import SaleForm
 from django.db.utils import IntegrityError
 
 class BaseTestCase(TestCase):
@@ -131,3 +134,142 @@ class SalesModelTestCase(BaseTestCase):
                 end_date=date.today() + timedelta(days=1),
             )
 
+class ViewsTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+        self.client.login(username='testuser', password='testpassword')
+
+        self.restaurant = Restaurant.objects.create(name="Test Restaurant")
+        self.dish_category = DishCategory.objects.create(name="Test Category")
+        self.dish = Dish.objects.create(name="Test Dish", restaurant=self.restaurant, price=10.0,category=self.dish_category)
+        self.sale = Sales.objects.create(
+            restaurant=self.restaurant,
+            title="Test Sale",
+            discount_percentage=10,
+            start_date=date.today() - timedelta(days=1),
+            end_date=date.today() + timedelta(days=1),
+        )
+        self.sale.dishes.add(self.dish)
+
+    def test_index_view(self):
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Featured Restaurants")
+        self.assertContains(response, "Special Sales")
+
+    def test_sale_detail_view(self):
+        response = self.client.get(reverse('sale_detail', args=[self.sale.id]))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Test Sale")
+        self.assertContains(response, "Test Dish")
+
+    def test_dish_categories_view(self):
+        response = self.client.get(reverse('dish_categories'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Test Category")
+
+    def test_restaurants_view(self):
+        response = self.client.get(reverse('restaurants'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Test Restaurant")
+
+    def test_restaurant_detail_view(self):
+        response = self.client.get(reverse('restaurant_detail', args=[self.restaurant.id]))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Test Restaurant")
+        self.assertContains(response, "Test Dish")
+        self.assertContains(response, "Test Sale")
+
+    def test_category_detail_view(self):
+        response = self.client.get(reverse('category_detail', args=[self.dish_category.id]))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Test Category")
+        self.assertContains(response, "Test Dish")
+
+
+class SaleFormTestCase(TestCase):
+    def setUp(self):
+        self.restaurant = Restaurant.objects.create(name="Test Restaurant")
+        self.dish = Dish.objects.create(name="Test Dish", restaurant=self.restaurant, price=10.0)
+        self.dish2 = Dish.objects.create(name="Test Dish2", restaurant=self.restaurant, price=10.0)
+        self.existing_sale = Sales.objects.create(
+            restaurant=self.restaurant,
+            title="Existing Sale",
+            discount_percentage=15,
+            start_date=date.today() - timedelta(days=1),
+            end_date=date.today() + timedelta(days=1),
+        )
+        self.existing_sale.dishes.add(self.dish)
+
+    def test_valid_form(self):
+        data = {
+            'restaurant': self.restaurant.id,
+            'title': 'New Sale',
+            'description': 'Test description',
+            'discount_percentage': 20,
+            'start_date': date.today() - timedelta(days=1),
+            'end_date': date.today() + timedelta(days=1),
+            'dishes': [self.dish2.id],
+        }
+        form = SaleForm(data=data)
+        self.assertTrue(form.is_valid())
+
+
+
+    # def test_invalid_form(self):
+    #     data = {
+    #         'restaurant': self.restaurant.id,
+    #         'title': '',
+    #         'description': '',
+    #         'discount_percentage': 20,
+    #         'start_date': date.today() + timedelta(days=1),
+    #         'end_date': date.today() + timedelta(days=2),
+    #         'dishes': [self.dish.id],
+    #     }
+    #     form = SaleForm(data=data)
+    #     self.assertFalse(form.is_valid())
+    #     print(f'{e}\n' for e in form.errors)
+    #     self.assertEqual(len(form.errors), 2)  # Two fields with errors
+
+    # def test_end_date_in_past(self):
+    #     data = {
+    #         'restaurant': self.restaurant.id,
+    #         'title': 'Invalid Sale',
+    #         'description': 'Test description',
+    #         'discount_percentage': 20,
+    #         'start_date': date.today() - timedelta(days=2),
+    #         'end_date': date.today() - timedelta(days=1),
+    #         'dishes': [self.dish.id],
+    #     }
+    #     form = SaleForm(data=data)
+    #     self.assertFalse(form.is_valid())
+    #     self.assertEqual(len(form.errors), 1)
+
+    # def test_dish_not_in_selected_restaurant(self):
+    #     data = {
+    #         'restaurant': self.restaurant.id,
+    #         'title': 'Invalid Sale',
+    #         'description': 'Test description',
+    #         'discount_percentage': 20,
+    #         'start_date': date.today() - timedelta(days=1),
+    #         'end_date': date.today() + timedelta(days=1),
+    #         'dishes': [self.dish.id],
+    #     }
+    #     form = SaleForm(data=data)
+    #     self.assertFalse(form.is_valid())
+    #     self.assertEqual(len(form.errors), 1)
+
+    # def test_existing_active_sale_for_dish(self):
+    #     data = {
+    #         'restaurant': self.restaurant.id,
+    #         'title': 'Invalid Sale',
+    #         'description': 'Test description',
+    #         'discount_percentage': 20,
+    #         'start_date': date.today() - timedelta(days=1),
+    #         'end_date': date.today() + timedelta(days=1),
+    #         'dishes': [self.dish.id],
+    #     }
+    #     form = SaleForm(data=data, instance=self.existing_sale)
+    #     self.assertFalse(form.is_valid())
+    #     self.assertEqual(len(form.errors), 1)
